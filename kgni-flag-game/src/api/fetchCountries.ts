4ef117e11129dfa570e7e9a3e774f24c4a84eddd
kgni <admin@mdia.dk>
2025-08-07 00:51:46 +0200

    add error handling to fetch countries and refactor

--- kgni-flag-game/src/api/fetchCountries.ts
+++ kgni-flag-game/src/api/fetchCountries.ts
@@ -1,16 +1,27 @@
 import { type Country, type CountryAPIData } from "../types/types";
+import { retry } from "./retry";
+import APIError from "./APIError";
+import { useFetchUrlStore } from "../stores/fetchURLStore";
 
-export default async function fetchCountries(): Promise<Country[]> {
-  const response = await fetch(
-    "https://restcountries.com/v3.1/all?fields=name,flags,capital",
-  );
+async function _fetchCountries(): Promise<Country[]> {
+  const url = useFetchUrlStore.getState().url;
+  const response = await fetch(url);
+  // Usage:
+  if (!response.ok) {
+    const msg =
+      response.status >= 500 && response.status < 600
+        ? "Something went wrong, please try again later."
+        : "Couldn't fetch countries. Please check your connection or try again.";
+    throw new APIError(msg, response.status);
+  }
   const data = await response.json();
 
-  const extendedData = data.map((country: CountryAPIData) => ({
+  const extendedData: Country[] = data.map((country: CountryAPIData) => ({
     ...country,
     isGuessed: false,
     answerOptions: getRandomOptions(data, country.name.common, 4),
   }));
+
   return randomSortResults(extendedData).slice(0, 3);
 }
 
@@ -33,9 +44,13 @@ function getRandomOptions(
     .map((c) => c.name.common)
     .filter((name) => name !== correct);
 
-  // Shuffle and pick 3
+  // Shuffle and  3
   const shuffled = randomSortResults(otherNames).slice(0, count - 1);
 
   // Add correct answer and shuffle again
   return randomSortResults([...shuffled, correct]);
+}
+
+export default function fetchCountriesWithRetry() {
+  return retry(_fetchCountries, 3, 1000); // 3 attempts, 1s delay
 }
