b7db800a9719051128c771f649977205ea86ab5b
kgni <admin@mdia.dk>
2025-08-07 21:08:22 +0200

    remove old code

--- kgni-flag-game/src/api/APIError.ts
+++ kgni-flag-game/src/api/APIError.ts
@@ -1,13 +1,3 @@
-// export default class APIError extends Error {
-//   status: number;
-//   constructor(message: string, status: number) {
-//     super(message);
-//     this.name = "FetchError";
-//     this.status = status;
-//   }
-// }
-//
-
 interface APIErrorOptions {
   message?: string;
   status: number;
